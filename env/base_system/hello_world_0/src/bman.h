2a22a058b416ab3e1375c2db92ee96844c884699
Aali132 <github@aali.ninjaloot.se>
2014-10-14 14:32:58 +0200

    B-man

---
+++ env/base_system/hello_world_0/src/bman.h
@@ -0,0 +1,29 @@
+#define G2s 425
+#define G2 450
+#define A2s 378
+#define F2 505
+#define A1 802
+
+#define BMAN_PULSE1_00 \
+{G2, HALF, SQR_WAVE}, \
+{G2s, QUART, SQR_WAVE}, \
+{A2s, EIGHT, SQR_WAVE}, \
+{G2, WHOLE, SQR_WAVE}, \
+{REST, EIGHT, SQR_WAVE}, \
+{F2, HALF, SQR_WAVE}, \
+{F2, QUART, SQR_WAVE}, \
+{G2, EIGHT, SQR_WAVE}, \
+{A1, EIGHT, SQR_WAVE}, \
+{G4, EIGHT, SQR_WAVE}, \
+{G4, EIGHT, SQR_WAVE}, \
+{C4s, QUART, SQR_WAVE}, \
+{G4, EIGHT, SQR_WAVE}, \
+{G4, EIGHT, SQR_WAVE}, \
+{C4s, QUART, SQR_WAVE}
+
+sample_t bubbleman = {
+
+BMAN_PULSE1_00,
+BMAN_PULSE1_00
+
+}
